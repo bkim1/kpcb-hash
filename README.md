5e256ff3a29ff5075d218f75179f7cbde6960a79
Branden Kim <brandenk0@gmail.com>
2017-09-07 14:32:11 -0400

    Create class skeleton

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 HashMap implementation for KPCB Engineering Fellows Program
 
 Written using Python v3.6.2
+
+
+Methods Required:
+
+* constructor(size): return an instance of the class with pre-allocated space for the given number of objects.
+* boolean set(key, value): stores the given key/value pair in the hash map. Returns a boolean value indicating success / failure of the operation.
+* get(key): return the value associated with the given key, or null if no value is set.
+* delete(key): delete the value associated with the given key, returning the value on success or null if the key has no value.
+* float load(): return a float value representing the load factor (`(items in hash map)/(size of hash map)`) of the data structure. Since the size of the dat structure is fixed, this should never be greater than 1.
---
+++ src/MyHashMap.py
@@ -0,0 +1,45 @@
+"""
+* HashMap implementation for KPCB Engineering Fellows Program
+"""
+
+
+class MyHashMap:
+    def __init__(self, size=100):
+        pass
+
+    def set(self, key, value):
+        """
+        * Stores given key:value pair in HashMap  
+
+        :return bool: Returns success/failure of key:value being set
+        """
+        pass
+
+    def get(self, key):
+        """
+        * Gets value found at given key
+
+        :return value: Returns value found at key 
+        """
+        pass
+
+    def delete(self, key):
+        """
+        * Deletes value found at given key
+
+        :return value: Returns value if successfully deleted
+        """
+        pass
+
+    def load(self):
+        """
+        * Finds the load factor of current tree
+        
+        :return float: Returns load factor of current HashMap
+        """
+        pass
+    
+
+    
+
+        
