7a993237504cf90dfaad29c273b22727f135991a
Branden Kim <brandenk0@gmail.com>
2017-09-17 21:11:03 -0400

    Add more unittests for HashMap

--- tests/test_myhashmap.py
+++ tests/test_myhashmap.py
@@ -2,7 +2,13 @@ import unittest
 from src.MyHashMap import MyHashMap
 
 class TestHashMap(unittest.TestCase):
-    def test_set_and_get(self):
+    def test_get(self):
+        hmap = MyHashMap()
+
+        with self.assertRaises(KeyError):
+            hmap.get("hi")
+    
+    def test_get_2(self):
         hmap = MyHashMap()
 
         hmap.set("test", 10)
@@ -10,12 +16,48 @@ class TestHashMap(unittest.TestCase):
         hmap.set("watsup", 50)
 
         self.assertTrue(hmap.get("test") == 10)
-
-    def test_get(self):
+    
+    def test_get_3(self):
         hmap = MyHashMap()
 
-        with self.assertRaises(KeyError):
-            hmap.get("hi")
+        with self.assertRaises(ValueError):
+            hmap.get(10)
+
+    def test_set(self):
+        hmap = MyHashMap()
+        hmap.set("test", 10)
+
+        self.assertEqual(len(hmap), 1)
+
+    def test_set_2(self):
+        hmap = MyHashMap()
+
+        with self.assertRaises(MemoryError):
+            for i in range(11):
+                hmap.set("t" + str(i), i)
+    
+    def test_set_3(self):
+        hmap = MyHashMap()
+
+        self.assertFalse(hmap.set(10, 10))    
+
+    def test_delete(self):
+        hmap = MyHashMap()
+
+        for i in range(8):
+            hmap.set("t" + str(i), i)
+        
+        self.assertEqual(hmap.delete("t2"), 2)
+
+    def test_delete_2(self):
+        hmap = MyHashMap()
+
+        for i in range(8):
+            hmap.set("t" + str(i), i)
+        
+        hmap.delete("t2")
+        self.assertEqual(len(hmap), 7)
+
 
 
 if __name__ == "__main__":
